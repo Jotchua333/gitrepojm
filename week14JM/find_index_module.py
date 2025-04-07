9fe173be7e2e09ced45303f5fca1806c9fa2afe7
Jotchua333 <JOM350@pitt.edu>
2025-04-07 13:45:32 -0400

    1

--- week14JM/find_index_module.py
+++ week14JM/find_index_module.py
@@ -3,7 +3,8 @@ Write a function that returns the index of a given element from the given list.
 '''
 print("Importing find index module")
 test_str = "test string"
-print('Running find index module directly: ', __name__)
+if __name__ == '__main__':
+    print('Running find index module directly: ', __name__)
 def find_index(search_list, target_elem):
     for idx, val in enumerate(search_list):
         #print(idx, val)
