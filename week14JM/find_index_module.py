1beffd2c6ece40d19ce7d44d205c78198f1c3032
Jotchua333 <JOM350@pitt.edu>
2025-04-07 13:26:04 -0400

    1

---
+++ week14JM/find_index_module.py
@@ -0,0 +1,26 @@
+'''
+Write a function that returns the index of a given element from the given list.
+'''
+print("Importing find index module")
+test_str = "test string"
+def find_index(search_list, target_elem):
+    for idx, val in enumerate(search_list):
+        #print(idx, val)
+        if val == target_elem:
+            return idx
+    return -1
+
+
+sample = ['apple', 'banana', 'orange', 'kiwi']
+print(find_index(sample, 'orange'))
+
+
+
+
+
+
+
+
+
+
+
