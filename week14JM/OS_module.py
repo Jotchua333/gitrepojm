f8f2bf90b964c4925e4b75999ac5d735f8f3c3f8
Jotchua333 <JOM350@pitt.edu>
2025-04-07 13:54:13 -0400

    1

---
+++ week14JM/OS_module.py
@@ -0,0 +1,40 @@
+'''
+OS Module - gives access to the local file system
+            and the underlying operating system
+'''
+import os
+#Get working directory:
+#print(os.getcwd())
+
+#Chnage directory:
+#os.chdir('C:\\Users\\JOM350\\OneDrive - University of Pittsburgh\\Spring2025\\PROGRAMMING')
+
+#List all files in current location:
+print(os.listdir())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
